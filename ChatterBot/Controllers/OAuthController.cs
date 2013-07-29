cef41b5d92b5dd5a4dfae49a71d1bb7a57b161f3
Steve Elliott <steve@tegud.net>
2013-07-29 07:16:45 +0100

    Start of OAuth

---
+++ ChatterBot/Controllers/OAuthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatterBot.Controllers
+{
+    public class OAuthController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("");
+        }
+        
+        public ActionResult Callback()
+        {
+            return Content("");
+        }
+    }
+}
